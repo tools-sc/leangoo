ccedb5aaee2ea7f39e1db705268c574eb59cff70
agzou <470841118@qq.com>
2017-09-05 11:11:19 +0800

    添加fastJson依赖

---
+++ src/main/java/com/team6/leangoo/conf/WebMvcConfigurer.java
@@ -0,0 +1,20 @@
+package com.team6.leangoo.conf;
+
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by AgZou on 2017/9/5.
+ */
+@Configuration
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        converters.add(converter);
+    }
+}
