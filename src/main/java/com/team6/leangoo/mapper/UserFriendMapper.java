b88ac2fae4b886b402682a5d0ef88fb9a1964af9
lfxiui <806941929@qq.com>
2017-09-07 18:10:21 +0800

    好友功能部分接口其他接口优化lfxiui2017/9/7

--- src/main/java/com/team6/leangoo/mapper/UserFriendMapper.java
+++ src/main/java/com/team6/leangoo/mapper/UserFriendMapper.java
@@ -3,5 +3,8 @@ package com.team6.leangoo.mapper;
 import com.team6.leangoo.model.UserFriend;
 import com.team6.leangoo.util.MyMapper;
 
+import java.util.List;
+
 public interface UserFriendMapper extends MyMapper<UserFriend> {
+    public List getFriendList(Integer userId);
 }
--- src/main/java/com/team6/leangoo/service/GetUserInfoService.java
+++ src/main/java/com/team6/leangoo/service/GetUserInfoService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Map;
+
 @Service
 @Transactional
 public class GetUserInfoService {
@@ -16,12 +18,20 @@ public class GetUserInfoService {
     public User getUserInfoById(Integer userId){
         return userMapper.selectByPrimaryKey(userId);
     }
-    public int changeUserInfo(User user){
+    public int changeUserInfo(User user,User userMsg){
+        user.setUserIntro(userMsg.getUserIntro());
+        user.setUserEmail(userMsg.getUserEmail());
+        user.setUserSex(userMsg.getUserSex());
         return userMapper.updateByPrimaryKey(user);
     }
-    public int changeUserPasssword(String userPassword,Integer userId){
-        User user = userMapper.selectByPrimaryKey(userId);
-        user.setUserPassword(userPassword);
-        return userMapper.updateByPrimaryKey(user);
+    public int changeUserPasssword(User user, Map map){
+        String oldPwd = map.get("oldPwd").toString();
+        String newPwd = map.get("newPwd").toString();
+        String newPwdConfirm = map.get("newPwdConfirm").toString();
+        if (user.getUserPassword().equals(oldPwd) && newPwd.equals(newPwdConfirm)){
+            user.setUserPassword(newPwd);
+            return userMapper.updateByPrimaryKey(user);
+        }else return 0;
+
     }
 }
